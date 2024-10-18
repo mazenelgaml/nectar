1c5ca4e875bba385ad922470f6f92652feede519
Salma Elbasha <157319687+SalmaElbasha@users.noreply.github.com>
2024-10-18 22:58:41 +0300

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:nectar/screens/beverages_screen/beverages_screen.dart';
 import 'package:nectar/screens/explore/explore_screen.dart';
+import 'package:nectar/screens/favorite/favorite_screen.dart';
+import 'package:nectar/screens/filter/filter_screen.dart';
 import 'package:nectar/screens/logingscreen/loging_screen.dart';
+import 'package:nectar/screens/mycarts_screen/my_carts_screen.dart';
 import 'package:nectar/screens/orderaccepted/order_accepted.dart';
 import 'package:nectar/screens/profilescreen/profile_screen.dart';
+import 'package:nectar/screens/search_screen/search_screen.dart';
 import 'package:nectar/screens/select_location/select_location_screen.dart';
 import 'package:nectar/screens/signupscreen/signup_screen.dart';
 import 'package:nectar/screens/splashscreen/splash_screen.dart';
@@ -18,7 +23,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter',
       debugShowCheckedModeBanner: false,
-      home:ExploreScreen(),
+      home:SplashScreen(),
     );
   }
 }
